8bdf88cb2de70abd3c455c45eba665c587babdfd
ajiebhaskoro <jie_bazz@yahoo.com>
2022-03-17 16:57:11 +0700

    added bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,7 +25,7 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ["password"]});
+// userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ["password"]});
 
 const User = new mongoose.model("users", userSchema);
 
@@ -36,16 +39,27 @@ app.get("/login", function(req, res){
 });
 
 app.post("/login", function(req, res){
+
     User.findOne(
         {email : req.body.username},
          function(err, results){
              if(!err){
                  if(results){
-                    if(results.password === req.body.password){
-                        res.render("secrets");
-                    } else {
-                        res.send("Wrong Password!")
-                    }
+
+                    bcrypt.compare(req.body.password, results.password, function(err, results){
+                        if(results === true){
+                            res.render("secrets");
+                        } else {
+                            res.send("Wrong Password!")
+                        }
+                    });
+
+                    // if(results.password === req.body.password){
+                    //     res.render("secrets");
+                    // } else {
+                    //     res.send("Wrong Password!")
+                    // }
+
                  } else {
                      res.send("Invalid Username/Password!")
                  }
@@ -60,19 +74,26 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email : req.body.username,
-        password : req.body.password
-    })
-
-    newUser.save(function(err, results){
+    
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
         if(!err){
-            res.render("secrets");
-        } else {
-            console.log(err);
-            res.send("Registration Failed!");
+
+            const newUser = new User({
+                email : req.body.username,
+                password : hash
+            })
+        
+            newUser.save(function(err, results){
+                if(!err){
+                    res.render("secrets");
+                } else {
+                    console.log(err);
+                    res.send("Registration Failed!");
+                }
+            })
         }
     })
+
 });
 
 
